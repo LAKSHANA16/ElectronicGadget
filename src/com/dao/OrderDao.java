0b0aa84540dd819d5c68527c36ce063ad0dce406
LAKSHANA16 <lakshanabala67@gmail.com>
2024-03-26 20:59:04 +0530

    Electronic gadget complete structure and test

---
+++ src/com/dao/OrderDao.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.model.OrdersModel;
+
+public interface OrderDao {
+	public List<OrdersModel> fetchAll() throws SQLException;
+	public void update(double totalAmount,int ordersId) throws SQLException;
+}
---
+++ src/com/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.dao.OrderDaoImpl;
+import com.model.OrdersModel;
+
+public class OrderService {
+OrderDaoImpl orderDaoImpl=new OrderDaoImpl();
+public List<OrdersModel> fetchAll() throws SQLException
+{
+return orderDaoImpl.fetchAll();
+}
+public void update(double totalAmount,int ordersId) throws SQLException
+{
+	orderDaoImpl.update(totalAmount, ordersId);
+}
+}
